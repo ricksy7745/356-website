d3757f00ef69c21b78f537180ae01344b21300a0
ricksy7745 <beckricks@gmail.com>
2024-03-25 22:59:37 -0600

    Finish the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,190 @@
-import logo from './logo.svg';
 import './App.css';
+import { sources } from "./Sources.js"
+import { research } from "./Research.js"
+import { body } from "./BodyText.js"
+
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function App() {
+  const everyoneInfo = sources.filter((source) => source.everyone === true)
+  const diagnosesInfo = sources.filter((source) => source.diagnoses === true);
+  const dailyInfo = sources.filter((source) => source.daily === true);
+  const crisisInfo = sources.filter((source) => source.crisis === true);
+  const bodyText = body;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="header-border">
+          <div className="header-container">
+            <div className="title-holder">
+              <h1>
+                <a className="header-text" href="/">UT Health Check</a>
+              </h1>
+              <h4 className='underline-text'>
+                <a className="header-text" href="/">Mental health resources for people needing answers</a>
+              </h4>
+            </div>
+            <div className="emergency-button">
+              <a className="emergency-text" target="_blank" rel="noreferrer" href="https://988lifeline.org/chat/">Expiriencing a mental health crisis?<br/> Click here or call 988</a>
+            </div>
+          </div>
+      </div>
+      <div className="body-container">
+        <div className="item-holder">
+          <div className="body-text">
+            <h3>{bodyText}</h3>
+          </div>
+          <div className="menu-holder">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+              >
+              <h3 className="menu-title">Daily practices for mental health disorder management</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+              <h4>Organized Practices</h4>
+              <ul>
+                {dailyInfo.filter((item) => item.practice === "organized").map((c) => (
+                  <li key={c.id} className="info-container">
+                      <div className="info">
+                        <p className="info-title">{c.title}:</p>
+                        <p className="info-description">{c.description}</p>
+                        <a className='info-link' href={c.link} target="_blank" rel="noreferrer">Click here for more information →</a>
+                      </div>
+                  </li>
+                ))}
+              </ul>
+              <h4>Personal Practices</h4>
+              <ul>
+                {dailyInfo.filter((item) => item.practice === "personal").map((c) => (
+                  <li key={c.id} className="info-container">
+                      <div className="info">
+                        <p className="info-title">{c.title}:</p>
+                        <p className="info-description">{c.description}</p>
+                        <a className='info-link' href={c.link} target="_blank" rel="noreferrer">Click here for more information →</a>
+                      </div>
+                  </li>
+                ))}
+              </ul>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className="menu-holder">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel3-content"
+                id="panel3-header"
+              >
+              <h3 className="menu-title">Mental health practices and info for everyone</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+              <h4>Organized Practices</h4>
+              <ul>
+                {everyoneInfo.filter((item) => item.practice === "organized").map((c) => (
+                  <li key={c.id} className="info-container">
+                      <div className="info">
+                        <p className="info-title">{c.title}:</p>
+                        <p className="info-description">{c.description}</p>
+                        <a className='info-link' href={c.link} target="_blank" rel="noreferrer">Click here for more information →</a>
+                      </div>
+                  </li>
+                ))}
+              </ul>
+              <h4>Personal Practices:</h4>
+              <ul>
+                {everyoneInfo.filter((item) => item.practice === "personal").map((c) => (
+                  <li key={c.id} className="info-container">
+                      <div className="info">
+                        <p className="info-title">{c.title}:</p>
+                        <p className="info-description">{c.description}</p>
+                        <a className='info-link' href={c.link} target="_blank" rel="noreferrer">Click here for more information →</a>
+                      </div>
+                  </li>
+                ))}
+              </ul>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className="menu-holder">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel4-content"
+                id="panel4-header"
+              >
+              <h3 className="menu-title">Emergency resources for those in crisis</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+              <ul>
+                {crisisInfo.map((c) => (
+                  <li key={c.id} className="info-container">
+                      <div className="info">
+                        <p className="info-title">{c.title}:</p>
+                        <p className="info-description">{c.description}</p>
+                        <a className='info-link' href={c.link} target="_blank" rel="noreferrer">Click here for more information →</a>
+                      </div>
+                  </li>
+                ))}
+              </ul>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className="menu-holder">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+              <h3 className="menu-title">Information on diagnosing mental health disorders</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+              <ul>
+                {diagnosesInfo.map((c) => (
+                  <li key={c.id} className="info-container">
+                      <div className="info">
+                        <p className="info-title">{c.title}:</p>
+                        <p className="info-description">{c.description}</p>
+                        <a className='info-link' href={c.link} target="_blank" rel="noreferrer">Click here for more information →</a>
+                      </div>
+                  </li>
+                ))}
+              </ul>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className="menu-holder">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel5-content"
+                id="panel5-header"
+              >
+              <h3 className="menu-title">Academic research on mental health</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+                <ul>
+                  {research.map((c) => (
+                    <li key={c.id} className="info-container">
+                        <div className="info">
+                          <p className="info-title">{c.title}:</p>
+                          <p className="info-description">{c.description}</p>
+                          {c.link && <a className='info-link' href={c.link} target="_blank" rel="noreferrer">Click here for more information →</a>}
+                        </div>
+                    </li>
+                  ))}
+                </ul>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Research.js
@@ -0,0 +1,20 @@
+export const research = [
+    {
+        id: 1,
+        title: "Understanding and Overcoming Procrastination",
+        description: "A study from Princeton University on procrastination.",
+        link: "https://mcgraw.princeton.edu/undergraduates/resources/resource-library/understanding-and-overcoming-procrastination",
+    },
+    {
+        id: 2,
+        title: "Mindfulness-Based Interventions for Anxiety and Depression",
+        description: "A study from Boston University on the benefits of mindfulness for depression and anxiety.",
+        link: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5679245/",
+    },
+    {
+        id: 3,
+        title: "How to Build Successful Habits and Improve Mental Health",
+        description: "A survey from Utah State University on mental health improvement.",
+        link: "https://extension.usu.edu/mentalhealth/articles/how-to-build-successful-habits-and-improve-mental-health#:~:text=Research%20shows%20that%20when%20people,both%20strengths%20and%20mental%20health.",
+    }
+]
